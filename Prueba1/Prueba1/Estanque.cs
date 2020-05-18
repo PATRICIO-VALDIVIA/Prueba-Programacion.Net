a511328cca3790cda888a19d6979c03bdb628152
PATRICIO-VALDIVIA <63594688+PATRICIO-VALDIVIA@users.noreply.github.com>
2020-05-18 17:47:03 -0400

    Update Estanque.cs

--- Prueba1/Prueba1/Estanque.cs
+++ Prueba1/Prueba1/Estanque.cs
@@ -6,30 +6,17 @@ namespace Prueba1
 {
     class Estanque : VehiculoComponentes
     {
-        private readonly double _Capacidad;
+       
         private double _litros;
-        public Estanque(double capacidad)
-        {
-
-            _Capacidad = Capacidad;
-        }
-
-        public double Capacidad => _Capacidad;
-
-        public double Litros
-        {
-            get => _litros;
-            set => double.TryParse(value.ToString(), out _litros);
-        }
-
-        public bool BajoCombustible()
-        {
-            return _litros <= 10.5;
-        }
-
-        public bool MitadCombustible()
+        
+        public bool MitadCombu()
         {
             return _litros > 10.5 && _litros <= 50.0;
         }
+         public bool BajoCombu()
+        {
+            return _litros <= 10.5;
+        }
+         
     }
 }
